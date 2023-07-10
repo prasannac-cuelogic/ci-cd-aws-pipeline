53ae8b9c0d5dfe3cfc62c60da212ee959dba0b38
prasanna champanerkar <prasanna.champanerkar@lntinfotech.com>
2023-07-10 20:55:43 +0530

    inital commit

---
+++ lib/ci-cd-aws-pipeline-stack.ts
@@ -0,0 +1,26 @@
+import * as cdk from 'aws-cdk-lib';
+import { CodePipelineSource, ShellStep, CodePipeline } from 'aws-cdk-lib/pipelines';
+import { Construct } from 'constructs';
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+export class CiCdAwsPipelineStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // The code that defines your stack goes here
+
+    // example resource
+    // const queue = new sqs.Queue(this, 'CiCdAwsPipelineQueue', {
+    //   visibilityTimeout: cdk.Duration.seconds(300)
+    // });
+    new CodePipeline(this, 'Pipeline', {
+      pipelineName: 'TestPipeline',
+      synth: new ShellStep('synth', {
+        input: CodePipelineSource.gitHub('prasannac-cuelogic/ci-cd-aws-pipeline', 'main'),
+        commands: ['npm ci',
+                   'npm run build',
+                   'npx cdk synth']
+      }),
+    });
+  }
+}
---
+++ test/ci-cd-aws-pipeline.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CiCdAwsPipeline from '../lib/ci-cd-aws-pipeline-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/ci-cd-aws-pipeline-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CiCdAwsPipeline.CiCdAwsPipelineStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
